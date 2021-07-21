7c43ee3705415b0f2781a0b53548a84529e92def
jucamilof <juancamilfonseca603@gmail.com>
2021-07-20 21:39:20 -0500

    Se agrego el formulario de registro

---
+++ app/src/main/java/com/example/proyectotiendavirtual/views/VistaRegistroDeUsuarioView.java
@@ -0,0 +1,20 @@
+package com.example.proyectotiendavirtual.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.example.proyectotiendavirtual.R;
+
+public class VistaRegistroDeUsuarioView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_registro_de_usuario_view);
+    }
+
+}
