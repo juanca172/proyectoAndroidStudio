03831a4ede5ecd7c1490a5b4aa81f70b6d8869cf
Dlufyace <Dlufyace@gmail.com>
2021-07-21 00:16:40 -0500

    se creo el cambio de activity para crear cuenta

--- app/src/main/java/com/example/proyectotiendavirtual/views/VistaIniciarSesion.java
+++ app/src/main/java/com/example/proyectotiendavirtual/views/VistaIniciarSesion.java
@@ -2,7 +2,9 @@ package com.example.proyectotiendavirtual.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.proyectotiendavirtual.R;
 
@@ -12,5 +14,11 @@ public class VistaIniciarSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_iniciar_sesion);
+    }
+
+    //Metodo para pasar a Crear Cuenta
+    public void Crear_Cuenta (View view){
+        Intent Crear_Cuenta = new Intent (this, VistaRegistroDeUsuarioView.class);
+        startActivity(Crear_Cuenta);
     }
 }
