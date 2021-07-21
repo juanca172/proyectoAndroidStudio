777feb3095588e0cdcbbb6e01bdafef701f95857
Dlufyace <Dlufyace@gmail.com>
2021-07-20 23:30:08 -0500

    se agrego el timer al la pagina inicial

--- app/src/main/java/com/example/proyectotiendavirtual/views/VistaPaginaInicial.java
+++ app/src/main/java/com/example/proyectotiendavirtual/views/VistaPaginaInicial.java
@@ -2,7 +2,9 @@ package com.example.proyectotiendavirtual.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.proyectotiendavirtual.R;
 
@@ -12,5 +14,24 @@ public class VistaPaginaInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_pagina_inicial);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goToMainActivity();
+            }
+        }, 2000);
+
+    }
+
+
+    public void goToMainActivity() {
+        Intent intent = new Intent(this, VistaIniciarSesion.class);
+        startActivity(intent);
+
     }
 }
