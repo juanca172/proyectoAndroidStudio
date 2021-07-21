352b91927721a290f080d4160d6e102399a9ac37
Dlufyace <Dlufyace@gmail.com>
2021-07-20 23:27:07 -0500

    seagrego iniciar sesion

---
+++ app/src/main/java/com/example/proyectotiendavirtual/views/VistaIniciarSesion.java
@@ -0,0 +1,16 @@
+package com.example.proyectotiendavirtual.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectotiendavirtual.R;
+
+public class VistaIniciarSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_iniciar_sesion);
+    }
+}
