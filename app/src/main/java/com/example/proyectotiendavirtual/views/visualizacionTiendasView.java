d3a1d15db96bce06516ce4b5267894ba48f2cd2a
jucamilof <juancamilfonseca603@gmail.com>
2021-07-24 21:18:17 -0500

    Hice el cambio del nombre de un layout a spinner_item_visualizacion_categorias y cree el array con los nombres de algunas tiendas y tambien empece a crear otro arrayAdapter

--- app/src/main/java/com/example/proyectotiendavirtual/views/visualizacionTiendasView.java
+++ app/src/main/java/com/example/proyectotiendavirtual/views/visualizacionTiendasView.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.example.proyectotiendavirtual.R;
@@ -11,16 +12,22 @@ import com.example.proyectotiendavirtual.R;
 public class visualizacionTiendasView extends AppCompatActivity {
     //encapsulamiento del spinner
     private Spinner spinner;
+    //encapsulamiento del listView
+    private ListView listado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizacion_tiendas_view);
         //Relacion entre la vista y la parte logica
         spinner = (Spinner)findViewById(R.id.PruebaSpinner);
-        //Creacion de un array
-        String [] things = {"Tecnologia", "Belleza y cuidado personal", "Articulos del hogar","televisores", "moda", "juegos y juguetes", "animales y mascotas"};
+        listado = (ListView)findViewById(R.id.list_view_1);
+        //Creacion de un array el cual tiene las categorias
+        String [] categorias = {"Tecnologia", "Belleza y cuidado personal", "Articulos del hogar","televisores", "moda", "juegos y juguetes", "animales y mascotas"};
+        String [] tiendas = {"Samsung", "Xiaomi", "falabella", "X hobbies", "articulosHogar.SAS"};
         //creacion de un adaptador para enviar datos al spinner
-        ArrayAdapter<String> arrayAdaptador = new ArrayAdapter<>(this, R.layout.spinner_item_visualizacion_tiendas, things);
+        ArrayAdapter<String> arrayAdaptador = new ArrayAdapter<>(this, R.layout.spinner_item_visualizacion_categorias, categorias);
         spinner.setAdapter(arrayAdaptador);
+        //creacion de un adaptador para enviar datos al listView
+        ArrayAdapter<String> adapterListado = new ArrayAdapter<>(this,)
     }
 }
