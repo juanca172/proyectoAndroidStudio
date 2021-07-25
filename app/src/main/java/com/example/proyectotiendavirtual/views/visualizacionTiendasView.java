9b5d92972842b29ede33282fe2f9369953e5ad59
jucamilof <juancamilfonseca603@gmail.com>
2021-07-24 20:39:33 -0500

    se agrego el spinner que muestra las categorias y otro layout para personalizar los items del spinner

--- app/src/main/java/com/example/proyectotiendavirtual/views/visualizacionTiendasView.java
+++ app/src/main/java/com/example/proyectotiendavirtual/views/visualizacionTiendasView.java
@@ -3,14 +3,24 @@ package com.example.proyectotiendavirtual.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.proyectotiendavirtual.R;
 
 public class visualizacionTiendasView extends AppCompatActivity {
-
+    //encapsulamiento del spinner
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizacion_tiendas_view);
+        //Relacion entre la vista y la parte logica
+        spinner = (Spinner)findViewById(R.id.PruebaSpinner);
+        //Creacion de un array
+        String [] things = {"Juan", "andres", "mercurio","sumar"};
+        //creacion de un adaptador para enviar datos al spinner
+        ArrayAdapter<String> arrayAdaptador = new ArrayAdapter<>(this, R.layout.spinner_item_visualizacion_tiendas, things);
+        spinner.setAdapter(arrayAdaptador);
     }
 }
