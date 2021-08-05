d1885d522030acebbd871fd046168ed581ad2132
jucamilof <juancamilfonseca603@gmail.com>
2021-08-04 20:02:01 -0500

    Se solucionaron conflictos en iniciar sesion layout y en iniciar sesion java

--- app/src/main/java/com/example/proyectotiendavirtual/views/VistaIniciarSesion.java
+++ app/src/main/java/com/example/proyectotiendavirtual/views/VistaIniciarSesion.java
@@ -49,23 +49,16 @@ public class VistaIniciarSesion extends AppCompatActivity {
 
     //metodo que verifica los dos metodos creados
     public void IniciarSesion(View view) {
-<<<<<<< HEAD
-=======
-
->>>>>>> 8e2003de923e8573d3cc1a6fd7d0a4a5aff7fb82
         ComparacionValores();
         ValidarCampoVacio();
 
         if (ComparacionValores()  && ValidarCampoVacio()) {
             Toast.makeText(this, "entro a iniciar sesion", Toast.LENGTH_SHORT).show();
-<<<<<<< HEAD
             /*variableSharedPreference = true;
             editor.putBoolean("seInicioSesion",variableSharedPreference);
             editor.apply();*/
-=======
         }else{
             Toast.makeText(this, "usuario y contraseña no corressponde", Toast.LENGTH_SHORT).show();
->>>>>>> 8e2003de923e8573d3cc1a6fd7d0a4a5aff7fb82
         }
     }
 
